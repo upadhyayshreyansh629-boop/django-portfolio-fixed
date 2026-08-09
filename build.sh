9059b8565443d3505e8e9a306acd5d1b8144c5b9
Shreyansh Upadhyay <upadhyayshreyansh629@gmail.com>
2026-08-09 09:11:34 +0530

    Load portfolio data during Render build

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py migrate
+
+python manage.py loaddata myapp_data.json
+
 python manage.py collectstatic --noinput
+
 python manage.py sync_cloudinary_media
